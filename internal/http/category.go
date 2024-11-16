1d28702b62506d1c76d600cffae3e4945cbd7b47
rulanugrh <nextarituenak01@gmail.com>
2024-11-17 00:38:41 +0700

    chore: adding comment in mail and category hander

--- internal/http/category.go
+++ internal/http/category.go
@@ -26,53 +26,68 @@ func NewCategoryHandler(service service.CategoryInterface) CategoryInterface {
 }
 
 func (ct *category) Create(c *fiber.Ctx) error {
+	// parsing request to body parser
 	var request domain.Category
 	err := c.BodyParser(&request)
+	// checking error while request body parser
 	if err != nil {
 		return c.Status(500).JSON(web.InternalServerError("Cannot Parsing Request"))
 	}
 
+	// parse reqeust to create from service layer
 	data, err := ct.service.Create(request)
+	// check error while create
 	if err != nil {
 		return c.Status(400).JSON(web.BadRequest(err.Error()))
 	}
 
+	// return success while create new category
 	return c.Status(201).JSON(web.Created("Success Create New Category", data))
 }
 
 func (ct *category) Delete(c *fiber.Ctx) error {
+	// get request id from url parameter
 	getID := c.Params("id")
-
+	// convert id string to integer
 	id, err := strconv.Atoi(getID)
 	if err != nil {
 		return c.Status(500).JSON(web.InternalServerError("Cannot Parsing ID"))
 	}
 
+	// process delete by id
 	err = ct.service.Delete(uint(id))
+	// checking error after deleted
 	if err != nil {
 		return c.Status(400).JSON(web.BadRequest(err.Error()))
 	}
 
+	// return success
 	return c.Status(200).JSON(web.Success("Success Delete Category", nil))
 }
 
 func (ct *category) Update(c *fiber.Ctx) error {
+	// parsing body request to entity
 	var request domain.Category
 	err := c.BodyParser(&request)
+	// check error after parsing body request
 	if err != nil {
 		return c.Status(500).JSON(web.InternalServerError("Cannot Parsing Body Request"))
 	}
 
+	// get id from url parameter
 	getID := c.Params("id")
+	// convert string to integer
 	id, err := strconv.Atoi(getID)
 	if err != nil {
 		return c.Status(500).JSON(web.InternalServerError("Cannot Parsing ID Request"))
 	}
 
+	// process update from request and parameter url
 	data, err := ct.service.Update(uint(id), request)
 	if err != nil {
 		return c.Status(400).JSON(web.BadRequest(err.Error()))
 	}
 
+	// return success
 	return c.Status(200).JSON(web.Success("Success Update Category", data))
 }
--- internal/http/mail.go
+++ internal/http/mail.go
@@ -27,44 +27,55 @@ func NewMailHandler(service service.MailInterface) MailInterface {
 }
 
 func (m *mail) Create(c *fiber.Ctx) error {
+	// parser body request to entity
 	var request domain.Mail
 	err := c.BodyParser(&request)
+	// check error if have error while create data
 	if err != nil {
 		return c.Status(500).JSON(web.InternalServerError("Error while parsing request"))
 	}
 
+	// process create to service layer
 	data, err := m.service.Create(request)
 	if err != nil {
 		return c.Status(400).JSON(web.BadRequest(err.Error()))
 	}
 
+	// return success
 	return c.Status(201).JSON(web.Created("Success create new Mail", data))
 }
 
 func (m *mail) GetAll(c *fiber.Ctx) error {
+	// get email from parameter
 	email := c.Params("email")
 
+	// process get data from email parameter
 	data, err := m.service.Get(email)
 	if err != nil {
 		return c.Status(400).JSON(web.BadRequest(err.Error()))
 	}
 
+	// return success
 	return c.Status(200).JSON(web.Success("Success get all mails by this email", data))
 }
 
 func (m *mail) GetByID(c *fiber.Ctx) error {
+	// get id from parameter
 	getID := c.Params("id")
 
+	// convert string into integer
 	id, err := strconv.Atoi(getID)
 	if err != nil {
 		return c.Status(500).JSON("Error while parsing ID")
 	}
 
+	// process get data from id parameter
 	data, err := m.service.FindByID(uint(id))
 	if err != nil {
 		return c.Status(400).JSON(err.Error())
 	}
 
+	// return success
 	return c.Status(200).JSON(web.Success("Success get mails by this id", data))
 }
 
